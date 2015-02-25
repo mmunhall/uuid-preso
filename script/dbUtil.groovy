e4297c73bed2d58aad58c07c6b6930620805d019
Mike Munhall <munhall.mike@gmail.com>
2015-02-25 09:52:57 -0700

    Started an example script.

---
+++ script/dbUtil.groovy
@@ -0,0 +1,23 @@
+//@GrabConfig(systemClassLoader=true)
+//@Grab(group='com.oracle', module='ojdbc6', version='11.2.0.2.0')
+
+
+@Grapes([
+    @Grab(group="com.google.guava", module="guava", version="18.0"),
+    @Grab(group='com.oracle', module='ojdbc6', version='11.2.0.2.0'),
+    @GrabConfig(systemClassLoader=true)
+])
+import oracle.jdbc.driver.OracleDriver
+import groovy.sql.Sql
+
+class dbUtil {
+
+    public static getSql() {
+        Sql.newInstance(
+            'jdbc:oracle:thin:@oraclehost:1521:devvm',
+            'uuid',
+            'uuid',
+            'oracle.jdbc.driver.OracleDriver'
+        )
+    }
+}
--- script/example.groovy
+++ script/example.groovy
@@ -1,9 +1,38 @@
-import java.util.UUID;
+import groovy.sql.Sql
+this.class.classLoader.rootLoader.addURL( new URL("file:////Users/mikemunhall/.m2/repository/com/oracle/ojdbc6/11.2.0.2.0/ojdbc6-11.2.0.2.0.jar") )
 
-UUID uuid = UUID.randomUUID();
 
-println(uuid.toString());
 
-UUID uuid2 = UUID.fromString(uuid.toString().toUpperCase().replace("-", ""));
+def rows = sql.rows("select id, rawtohex(id) idAsHex, first_name, last_name from people")
 
-println(uuid2.toString());
+println "id (raw): ${rows[0].id}"
+println "id (hex): ${rows[0].idAsHex}"
+
+
+
+
+
+
+/*def operators = sql.rows("select * from operator order by id").collect { it.id }
+def feeds = sql.rows("select * from feed order by id").collect { it.id }
+
+def combined = GroovyCollections.combinations([operators, feeds])
+combined.each {
+    println """<OPERATOR_FEED OPERATOR_ID="${it[0]}" FEED_ID="${it[1]}" GOAL_ALLOCATION="1" CIP_ENABLED="1"/>"""
+}*/
+
+
+
+
+
+
+
+
+def private getSql() {
+    Sql.newInstance(
+        'jdbc:oracle:thin:@oraclehost:1521:devvm',
+        'uuid',
+        'uuid',
+        'oracle.jdbc.driver.OracleDriver'
+    )
+}
